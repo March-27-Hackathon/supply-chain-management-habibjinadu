b34fd5c08938d9ff0dfaa8be6491f885093368c5
Carter Fuchs <carterjfuchs@gmail.com>
2021-03-27 13:50:34 -0600

    Started creating the LowestCost class

--- src/edu/ucalgary/ensf409/LowestCost.java
+++ src/edu/ucalgary/ensf409/LowestCost.java
@@ -1,4 +1,20 @@
 package edu.ucalgary.ensf409;
 
+import java.sql.*;
+
+/**
+ * Uses a connection to a database to calculate the lowest cost of creating a furniture item, given the desired furniture
+ * item and its type.
+ */
 public class LowestCost {
+    private Connection dbConnect;
+    private String furnitureType;
+    private int numberOfItems;
+
+    public LowestCost(Connection dbConnect, String fType, int numItems) {
+        this.dbConnect = dbConnect;
+        this.furnitureType = fType;
+        this.numberOfItems = numItems;
+    }
+    
 }
