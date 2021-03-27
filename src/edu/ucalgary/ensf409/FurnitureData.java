21abd8abcf7b06445cdf1c7b36dd3dc0c3df55c2
Habib Jinadu <adesolajinadu@gmail.com>
2021-03-27 13:30:42 -0600

    added furniture database connection in FurnitureData

--- src/edu/ucalgary/ensf409/FurnitureData.java
+++ src/edu/ucalgary/ensf409/FurnitureData.java
@@ -1,4 +1,48 @@
 package edu.ucalgary.ensf409;
 
+import java.sql.*;
 public class FurnitureData {
+
+    public final String DBURL; //store the database url information
+    public final String USERNAME; //store the user's account username
+    public final String PASSWORD; //store the user's account password
+
+    public Connection databaseConnection; // variable to store the database 
+    // connection
+
+    public FurnitureData(String url, String userName, String password)
+    {
+        this.DBURL = url; // initialize the database url with the url argument
+        // initialize USERNAME with the USERNAME argument
+        this.USERNAME = userName; 
+        // initialize PASSWORD with the password argument
+        this.PASSWORD = password;
+    }
+
+    /**
+     * creates a connection with the database
+     */
+    public void initializeConnection()
+    {
+        try 
+        {
+            // connect to the database
+            this.databaseConnection = DriverManager.getConnection(this.DBURL, 
+                                                this. USERNAME,this.PASSWORD);
+        }
+        catch(SQLException e)
+        {
+            // print error message
+            System.out.println("Cannot connect to the database");
+            e.printStackTrace(); // print stack trace
+        }
+        
+    }
+    
+    public Connection getDatabaseConnection()
+    {
+        return this.databaseConnection; // return the database connection
+    }
+       
+
 }
--- src/edu/ucalgary/ensf409/Main.java
+++ src/edu/ucalgary/ensf409/Main.java
@@ -1,7 +1,15 @@
 package edu.ucalgary.ensf409;
+import java.sql.*;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("Test");
+
+        // Habib's Test Code
+        FurnitureData database = new FurnitureData("jdbc:mysql://localhost/inventory","habib","password");
+
+        database.initializeConnection(); // initialize the connection
+        
+        Connection databaseConnection = database.getDatabaseConnection();
     }
 }
