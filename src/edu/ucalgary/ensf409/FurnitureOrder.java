a77a73d1937d63d6ca8b57097f2c0e26dc010765
Habib Jinadu <adesolajinadu@gmail.com>
2021-04-13 18:13:49 -0600

    prepared for submission

--- src/edu/ucalgary/ensf409/FurnitureOrder.java
+++ src/edu/ucalgary/ensf409/FurnitureOrder.java
@@ -34,25 +34,41 @@ public class FurnitureOrder {
     // for yes, false for no
 
     public FurnitureOrder(String category, String type, int numItems) {
-        furnitureIDList = new LinkedList<>();
-        manufacturerNameList = null;
-        price = 0;
-        CATEGORY = category;
-        TYPE = type;
-        NUMITEMS = numItems;
-        fulfilled = false;
+        furnitureIDList = new LinkedList<>(); // make new linkedList to hold ids
+        manufacturerNameList = null; // set the manufacturer name list to null
+        price = 0; // set the price of the order to zero
+        CATEGORY = category; // assign the category argument to CATEGORY
+        TYPE = type; // assign the type argument to TYPE
+        NUMITEMS = numItems; // assign numItems to NUMITEMS
+        fulfilled = false; // set fulfilled to false
     }
 
+    /**
+     * Get the furnitureIDList data member
+     * @return a linkedList that contains the id of the furniture list as a 
+     * string
+     */
     public LinkedList<String> getFurnitureIDList() {
-        return furnitureIDList;
+        return furnitureIDList; // return the furnitureID list data member
     }
 
+    /**
+     * get the manufacturerNameList
+     * @return
+     */
     public LinkedList<String> getManufacturerNameList() {
-        return manufacturerNameList;
+        // return the manufacturerNameList data member
+        return manufacturerNameList; 
     }
 
+    /**
+     * set the ManufacturerNameList 
+     * @param list LinkedList of strings that contain the manufacturer names 
+     * for the order
+     */
     public void setManufacturerNameList(LinkedList<String> list) {
-        manufacturerNameList = list;
+        // assign the manufacturerNameList data member to the list LinkedList
+        manufacturerNameList = list; 
     }
 
     /**
@@ -60,36 +76,68 @@ public class FurnitureOrder {
      * @param id The ID to add to the list.
      */
     public void addID(String id) {
-        furnitureIDList.add(id);
+        furnitureIDList.add(id); // add the id to the furnitureIDList
     }
+
+    /**
+     * get the price data member
+     * @return an int representing the price of the furniture order. It will be
+     * 0 is there are no items in the furniture order
+     */
     public int getPrice() {
-        return price;
+        return price; // return the price data member
     }
 
+    /**
+     * set the price data member in the furnitureOrder
+     * @param price int representing the price of the items in the 
+     * furnitureIDList
+     */
     public void setPrice(int price) {
-        this.price = price;
+        // assign the price data member to the price argument
+        this.price = price; 
     }
 
+    /**
+     * get the CATEGORY data member
+     * @return a String representing the category of the items that are stored
+     * in the FurnitureOrder
+     */
     public String getCATEGORY() {
-        return CATEGORY;
+        return CATEGORY; // return the CATEGORY data member
     }
 
+    /**
+     * get the TYPE data member
+     * @return a String representing the type of items that are stored
+     * in the FurnitureOrder
+     */
     public String getTYPE() {
-        return TYPE;
+        return TYPE; // return the TYPE data member
     }
 
+    /**
+     * get the NUMITEMS data member
+     * @return an int representing the number of items that are stored
+     * in the FurnitureOrder
+     */
     public int getNUMITEMS() {
-        return NUMITEMS;
+        return NUMITEMS; // return the NUMITEMS data member
     }
 
+    /**
+     * checks to see if the order has been fulfilled.
+     * @return a boolean representing of the items in the furniture list is the
+     * lowest cost combination of parts that will fulfill the client order
+     */
     public boolean isFulfilled() {
-        return fulfilled;
+        return fulfilled; // return the fulfilled data member
     }
 
     /**
      * Marks the order as fulfilled.
      */
     public void setFulfilled() {
-        fulfilled = true;
+        fulfilled = true; // ser fulfilled to true
     }
 }
